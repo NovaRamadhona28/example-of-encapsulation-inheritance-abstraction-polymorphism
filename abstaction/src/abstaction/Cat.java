b1e6602b80ebe9c1acc40693ecaaca6ef5d1aa75
NovaRamadhona28 <58648266+NovaRamadhona28@users.noreply.github.com>
2019-12-19 15:45:44 +0700

    Add files via upload

---
+++ abstaction/src/abstaction/Cat.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstaction;
+
+/**
+ *
+ * @author Lintang Pramesti
+ */
+public class Cat extends Animal{
+
+    @Override
+    public void animalSound() {
+        System.out.println("The cat says: meow meow");
+    }
+    
+}
---
+++ abstaction/src/abstaction/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstaction;
+
+/**
+ *
+ * @author Nova Ramadhona
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Cat cat = new Cat(); //Create a Cat object
+        cat.animalSound();  
+        cat.sleep();
+    }
+    
+}
